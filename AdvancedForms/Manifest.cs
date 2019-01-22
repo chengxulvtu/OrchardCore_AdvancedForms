f47f3b097287cf976563cd03e7f656e8f892946e
MohitNaroliya <mohit.naroliya@arnasoftech.com>
2019-01-22 11:03:04 +0530

    Recipe Creation #2

--- AdvancedForms/Manifest.cs
+++ AdvancedForms/Manifest.cs
@@ -4,7 +4,7 @@
     Name = "Advanced Forms Module",
     Author = "Matt Kenney",
     Version = "0.0.1",
-    Description = "Creates forms content types, uses Forms.IO open source MIT open source tools.",
+    Description = "Creates forms content types, uses Forms.IO open source MIT open source tools. NOTE: Turn on Query Module before turning on this module.",
     Category = "MRTD"
 )]
 
