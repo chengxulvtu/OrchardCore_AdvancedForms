4b9ca987ba52158cb54c7fa1e7d8152f89ba659c
Matt kenney <matt.kenney@gmail.com>
2019-01-22 22:52:45 -0600

    Update Manifest File, update to latest version of OrchardCore myget projects

--- AdvancedForms/Manifest.cs
+++ AdvancedForms/Manifest.cs
@@ -3,8 +3,16 @@
 [assembly: Module(
     Name = "Advanced Forms Module",
     Author = "Matt Kenney",
-    Version = "0.0.1",
-    Description = "Creates forms content types, uses Forms.IO open source MIT open source tools. NOTE: Turn on Query Module before turning on this module.",
-    Category = "MRTD"
+    Version = "0.1",
+    Website = "https://github.com/themattkenney/OrchardCore_AdvancedForms"
 )]
+
+[assembly: Feature(
+    Id = "AdvancedForms",
+    Name = "AdvancedForms",
+    Description = "Creates forms content types, uses Forms.IO open source MIT open source tools. NOTE: Turn on Query Module before turning on this module.",
+    Dependencies = new[] { "OrchardCore.Contents, OrchardCore.Lists" },
+    Category = "MRT Software"
+)]
+
 
