ea2e7e825793f2ecc9b446ae730a8620a8d18809
MohitNaroliya <mohit.naroliya@arnasoftech.com>
2019-01-24 16:57:09 +0530

    Setup Demo Site on Azure

--- cms.app/Startup.cs
+++ cms.app/Startup.cs
@@ -21,10 +21,11 @@ namespace cms.app
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            app.UseDeveloperExceptionPage();
 
             app.UseOrchardCore();
         }
