f6cd1ce5da89ab743f85d8671e1687e463854313
MohitNaroliya <mohit.naroliya@arnasoftech.com>
2019-01-28 18:45:30 +0530

    Create an Advanced Form Type content type#2

---
+++ AdvancedForms/ViewModels/ContentPickerItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvancedForms.ViewModels
+{
+    public class ContentPickerItemViewModel
+    {
+        public string ContentItemId { get; set; }
+        public string DisplayText { get; set; }
+        public bool HasPublished { get; set; }
+    }
+}
