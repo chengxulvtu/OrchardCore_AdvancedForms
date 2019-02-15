2310d5c40f37c1337ae930d3b0fdf2dc30badc90
MohitNaroliya <mohit.naroliya@arnasoftech.com>
2019-02-15 12:29:27 +0530

    Moved JS to separate file for unfinished draft

---
+++ AdvancedForms/wwwroot/Scripts/Display.js
@@ -0,0 +1,56 @@
+﻿var Title, Id, Header, Footer, Description, Type, SubmissionId, Instructions, Owner, AdminContainer, DraftSubmission;
+
+function initValue(title, id, header, footer, description, type, submissionId, instructions, owner, adminContainer) {
+    debugger;
+    Title = title;
+    Id = id;
+    Header = header;
+    Footer = footer;
+    Description = description;
+    Type = type;
+    SubmissionId = submissionId;
+    Instructions = instructions;
+    Owner = owner;
+    AdminContainer = adminContainer;
+}
+
+function saveDraft() {
+    saveForm(DraftSubmission.data, true);
+}
+
+function saveForm(submission, isDraft) {
+    debugger;
+    $.ajax({
+        url: '/AdvancedForms/Entry',
+        method: 'POST',
+        data: {
+            __RequestVerificationToken: $("input[name='__RequestVerificationToken']").val(),
+            submission: JSON.stringify(submission),
+            Title: Title,
+            id: Id,
+            container: document.getElementById('Container').value,
+            header: Header,
+            footer: Footer,
+            description: Description,
+            type: Type,
+            submissionId: SubmissionId,
+            instructions: Instructions,
+            owner: Owner,
+            adminContainer: AdminContainer,
+            adminSubmission: document.getElementById('AdminSubmission').value,
+            isDraft: isDraft
+        },
+        success: function (data) {
+            window.location.replace("/submission-confirmation");
+        },
+        error: function (error) {
+            var errorMsg = "Unable to Save. Try again later.";
+            $('<div class="alert alert-danger" role="alert"></div>').text(errorMsg + error.responseText).appendTo($('#advancedForm-errors'));
+        }
+    });
+}
+
+function printButton() {
+    window.open(window.location.href.toLowerCase().replace('/edit/', '/print/').replace('/view/', '/print/'), '_blank');
+};
+
